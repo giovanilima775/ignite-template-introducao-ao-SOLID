1fd22fd0e49097aa0c2f9a63bddb05a41341b686
Giovani Lima <giovanilima775@hotmail.com>
2021-11-12 23:04:15 -0300

    feat: resolved

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -2,14 +2,23 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+  user_id?: string;
 }
 
 class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("Don't exist this user.");
+    }
+    if (!user.admin) {
+      throw new Error("This user not is admin");
+    }
+    
+    return this.usersRepository.list();
   }
 }
 
